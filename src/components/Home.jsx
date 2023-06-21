34b8700b2cfdee4c2a179da1a3f37abc9ff86e1b
Patrick Mo <patrick@MacBook-Pro.local>
2023-06-22 07:07:29 +0800

    Added and integrated portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,62 @@
 import React, { useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Room from "./Room";
-import IconNavBar from "./IconNavBar";
+
+import { useNavigate, useLocation } from "react-router-dom";
+import portfolioImage from "assets/portfolio.png";
+import profileImage from "assets/profile.png";
+import coworkersImage from "assets/coworkers.png";
+
+import Portfolio from "./portfolio";
 
 import "./Home.css";
 
 const Home = () => {
   const [userData] = useLocalStorage("userData", {});
-  
+  const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState(null);
+  const [portfolioVisible, setPortfolioVisible] = useState(false);
+
+  const handleButtonClick = (route) => {
+    navigate(route, { replace: true });
+    setActiveButton(route);
+  };
+
+  const togglePortfolioVisibility = () => {
+    setPortfolioVisible(!portfolioVisible);
+  };
+
   return (
     <>
       <div className="home">
         <h1 className="welcome">Welcome back, {userData.username}!</h1>
         <Room />
-        <IconNavBar></IconNavBar>
+        <div className="image-container">
+          <button
+            className={`image-button ${activeButton === '/' ? 'active' : ''}`}
+            onClick={togglePortfolioVisibility}
+          >
+            <img src={portfolioImage} alt="" className="image" />
+          </button>
+          <button
+            className={`image-button ${activeButton === "/Rooms" ? "active" : ""}`}
+            onClick={() => handleButtonClick("/Rooms")}
+          >
+            <img src={profileImage} alt="" className="image" />
+          </button>
+          <button
+            className={`image-button ${activeButton === "/Rooms" ? "active" : ""}`}
+            onClick={() => handleButtonClick("/Rooms")}
+          >
+            <img src={coworkersImage} alt="" className="image" />
+          </button>
+        </div>
+
+        {portfolioVisible && (
+          <div className="portfolio-container">
+            <Portfolio className="portfolio-view" username={userData.username}/>
+          </div>
+        )}
       </div>
     </>
   );
---
+++ src/components/portfolio.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import "./portfolio.css";
+
+const LeftBranch = ({ event, description }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="LeftBranch">
+      <div className="Header" onClick={() => setShow(!show)}>
+        <span className="HeaderText">{event}</span> <br />
+        {show === true && <p className="moreInfo">{description}</p>}
+      </div>
+
+      <div className="node-container">
+        <div className="node-stick" />
+        <div className="node-circle" />
+        <div className="node-branch" />
+      </div>
+    </div>
+  );
+};
+
+const RightBranch = ({ event, description }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="RightBranch">
+      <div
+        className="Right-Header"
+        onClick={() => setShow(!show)}
+      >
+        <span className="HeaderText">{event}</span> <br />
+        {show === true && <p className="moreInfo">{description}</p>}
+      </div>
+
+      <div className="node-container">
+        <div className="node-stick" />
+        <div className="node-circle" />
+        <div className="right-node-branch" />
+      </div>
+    </div>
+  );
+};
+
+const Portfolio = (props) => {
+  return (
+    <div className="Portfolio_Tree">
+      <h1>Here's what {props.username}'s been up to.</h1>
+      <LeftBranch event="Code EXP 2023" description="Did some Programming in CodeEXP 2022, ngl was one of the best hackathons ever."/>
+      <RightBranch event="What The Hack 2023" description="Not as cool as Code EXP but welp, there can only be one."/>
+      <LeftBranch event="Halloween Hacks 2023" description="Very spooky hackathon, pretty cool actually!"/>
+    </div>
+  );
+};
+
+export default Portfolio;
