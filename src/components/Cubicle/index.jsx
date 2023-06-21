aff68c5b91f4b4c97cc07875515e0f99a6f9f55b
pyraxo <pyraxo@pyraxo.moe>
2023-06-21 14:53:01 +0800

    Minor fixes

--- src/components/Cubicle/index.jsx
+++ src/components/Cubicle/index.jsx
@@ -1,31 +1,40 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Phaser from "phaser";
-import tilemap from "assets/iso-64x64-outside.png";
-import tilemap2 from "assets/iso-64x64-building.png";
-import isorpg from "assets/isorpg.json";
+// import tilemap from "assets/iso-64x64-outside.png";
+// import tilemap2 from "assets/iso-64x64-building.png";
+// import isorpg from "assets/isorpg.json";
+import tilemap from "../../assets/iso-sandbox.png";
+import sandbox from "assets/sandbox.json";
+import "./style.css";
 
 class CubicleScene extends Phaser.Scene {
   preload() {
+    // this.load.image("tiles", tilemap);
+    // this.load.image("tiles2", tilemap2);
+    // this.load.tilemapTiledJSON("map", isorpg);
     this.load.image("tiles", tilemap);
-    this.load.image("tiles2", tilemap2);
-    this.load.tilemapTiledJSON("map", isorpg);
+    this.load.tilemapTiledJSON("map", sandbox);
   }
 
   create() {
     const map = this.add.tilemap("map");
 
-    const tileset1 = map.addTilesetImage("iso-64x64-outside", "tiles");
-    const tileset2 = map.addTilesetImage("iso-64x64-building", "tiles2");
+    // const tileset1 = map.addTilesetImage("iso-64x64-outside", "tiles");
+    // const tileset2 = map.addTilesetImage("iso-64x64-building", "tiles2");
 
-    map.createLayer("Tile Layer 1", [tileset1, tileset2]);
-    map.createLayer("Tile Layer 2", [tileset1, tileset2]);
-    map.createLayer("Tile Layer 3", [tileset1, tileset2]);
-    map.createLayer("Tile Layer 4", [tileset1, tileset2]);
-    map.createLayer("Tile Layer 5", [tileset1, tileset2]);
+    // map.createLayer("Tile Layer 1", [tileset1, tileset2]);
+    // map.createLayer("Tile Layer 2", [tileset1, tileset2]);
+    // map.createLayer("Tile Layer 3", [tileset1, tileset2]);
+    // map.createLayer("Tile Layer 4", [tileset1, tileset2]);
+    // map.createLayer("Tile Layer 5", [tileset1, tileset2]);
+
+    const tileset = map.addTilesetImage("isometric-sandbox-sheet", "tiles");
+    map.createLayer("Tile Layer 1", [tileset]);
+
+    // this.cameras.main.centerOn(0, 150);
+    this.cameras.main.centerOn(0, 150);
 
     const cursors = this.input.keyboard.createCursorKeys();
-
-    this.cameras.main.setZoom(2);
 
     const controlConfig = {
       camera: this.cameras.main,
@@ -49,10 +58,11 @@ class CubicleScene extends Phaser.Scene {
 }
 
 const config = {
-  type: Phaser.WEBGL,
-  width: 800,
-  height: 800,
+  type: Phaser.AUTO,
+  width: 600,
+  height: 600,
   pixelArt: true,
+  parent: "phaser-container",
   scene: CubicleScene,
   transparent: true,
 };
@@ -67,7 +77,7 @@ const Cubicle = () => {
     };
   }, []);
 
-  return <div id="phaser-container"></div>;
+  return <div id="phaser-container" />;
 };
 
 export default Cubicle;
