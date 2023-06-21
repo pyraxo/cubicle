46dfe78509c452772ede6e391c46cb0d5df9774d
pyraxo <pyraxo@pyraxo.moe>
2023-06-22 04:54:15 +0800

    Add blur colour effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,20 @@ import React, { useEffect } from "react";
 
 import useLocalStorage from "../hooks/useLocalStorage";
 import Room from "./Room";
+
 import portfolioImage from "../assets/portfolio.png";
 import profileImage from "../assets/profile.png";
 import coworkersImage from "../assets/coworkers.png";
 
 import "./Home.css";
+import Blur from "./Blur";
 
 const Home = () => {
   const [userData] = useLocalStorage("userData", {});
 
   return (
     <>
+      <Blur />
       <div className="home">
         <h1 className="welcome">Welcome back, {userData.username}!</h1>
         <Room />
